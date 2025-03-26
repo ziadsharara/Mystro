cfdee4c2e27c7f7149415626151704e94186cf20
Eldeeb <ziad.shararaa@gmail.com>
2025-03-26 06:45:26 +0200

    remove temps

--- Mystro.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Mystro.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -21,7 +21,7 @@ namespace Mystro.DataAccess.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("BulkyWeb.Models.Category", b =>
+            modelBuilder.Entity("Mystro.Models.Models.Category", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -34,7 +34,8 @@ namespace Mystro.DataAccess.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
--- Mystro.DataAccess/obj/Debug/net9.0/Mystro.DataAccess.AssemblyInfo.cs
+++ Mystro.DataAccess/obj/Debug/net9.0/Mystro.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mystro.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb11a667db3edd1fe8d76f3298452472fd0054df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58eddf3556e634fe12e76567f5882f985166000c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mystro.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mystro.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
